d668bbf12f3a3ce9c237fff129a05fa89c9f7a35
vityaman <vityaman.dev@yandex.ru>
2024-05-14 20:51:21 +0300

    Initialize the project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# mcst-internship-trial
+# MCST Internship Trial
+
+## Build & Run
+
+You can build it as a usual CMake project
+with
+
+```bash
+(cmake -S . -B build && cmake --build build)
+```
+
+For a developement consider using provided 
+`Makefile`, as it automates some routine tasks.
---
+++ test/test.cpp
@@ -0,0 +1,21 @@
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
+
+namespace filehash::test {
+
+auto factorial(uint32_t number) -> uint32_t {
+  return number <= 1 ? number : factorial(number - 1) * number;
+}
+
+namespace {
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+  REQUIRE(factorial(1) == 1);
+  REQUIRE(factorial(2) == 2);
+  REQUIRE(factorial(3) == 6);
+  REQUIRE(factorial(10) == 3'628'800);
+}
+
+} // namespace
+
+} // namespace filehash::test
