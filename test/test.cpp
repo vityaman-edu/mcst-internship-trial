26b34c6a4e2f628b5078e3786c0068068e2964f0
vityaman <vityaman.dev@yandex.ru>
2024-05-15 19:49:33 +0300

    Extract test input generator

--- test/test.cpp
+++ test/test.cpp
@@ -9,46 +9,57 @@
 #include <ostream>
 #include <random>
 #include <sstream>
+#include <utility>
 #include <vector>
 
 namespace filehash::test {
 
-TEST_CASE("In-memory same as data_processor") {
-  using filehash::smart::BlockSizeElements;
-  using filehash::smart::Hash;
+using filehash::smart::BlockSizeElements;
 
-  constexpr std::size_t seed = 1232142132;
+auto GenerateInput(std::default_random_engine& random)
+    -> std::pair<std::stringstream, std::vector<std::uint32_t>> {
+
   constexpr std::size_t zero_freq = 10;
 
-  std::default_random_engine random{seed}; // NOLINT
-
-  std::uniform_int_distribution<std::uint8_t> byte_dist{
+  static std::uniform_int_distribution<std::uint8_t> byte_dist{
       std::numeric_limits<std::uint8_t>::min(),
       std::numeric_limits<std::uint8_t>::max(),
   };
 
-  std::uniform_int_distribution<std::uint8_t> is_zero_dist{0, zero_freq};
+  static std::uniform_int_distribution<std::uint8_t> is_zero_dist{0, zero_freq};
 
-  std::uniform_int_distribution<std::size_t> size_dist{
+  static std::uniform_int_distribution<std::size_t> size_dist{
       0, 3 * BlockSizeElements
   };
+
+  const auto size = size_dist(random);
+
+  std::stringstream stream;
+
+  std::vector<std::uint32_t> block(size);
+  auto* buffer = reinterpret_cast<std::uint8_t*>(block.data()); // NOLINT
+
+  for (std::size_t j = 0; j < size; ++j) {
+    const bool is_zero = is_zero_dist(random) > 0;
+    const std::uint8_t byte = is_zero ? 0 : byte_dist(random);
+    stream << byte;
+    buffer[j] = byte; // NOLINT
+  }
+
+  return std::make_pair(std::move(stream), std::move(block));
+}
+
+TEST_CASE("In-memory same as data_processor") {
+  using filehash::smart::Hash;
+
+  constexpr std::size_t seed = 1232142132;
+
+  std::default_random_engine random{seed}; // NOLINT
 
   constexpr std::size_t rounds = 500;
   constexpr std::size_t batch = 5;
   for (std::size_t i = 0; i < rounds; ++i) {
-    const auto size = size_dist(random);
-
-    std::stringstream stream;
-
-    std::vector<std::uint32_t> block(size);
-    auto* buffer = reinterpret_cast<std::uint8_t*>(block.data()); // NOLINT
-
-    for (std::size_t j = 0; j < size; ++j) {
-      const bool is_zero = is_zero_dist(random) > 0;
-      const std::uint8_t byte = is_zero ? 0 : byte_dist(random);
-      stream << byte;
-      buffer[j] = byte; // NOLINT
-    }
+    auto [stream, block] = GenerateInput(random);
 
     // FIXME: is this okay that, if p := process_block
     // p(concat(a, b, c)) == p(a) then p(b) then p(c)?
@@ -58,8 +69,8 @@ TEST_CASE("In-memory same as data_processor") {
 
     if (i % batch == 0) {
       WARN(
-          "Testing iteration " << i << " with size " << size << " and hash "
-                               << actual << "..."
+          "Testing iteration " << i << " with size " << block.size()
+                               << " and hash " << actual << "..."
       );
     }
   }
