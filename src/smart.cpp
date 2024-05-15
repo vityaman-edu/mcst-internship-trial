5493d9187e31763686f71eba5b9c1ea6630506fa
vityaman <vityaman.dev@yandex.ru>
2024-05-15 17:19:41 +0300

    Add zeroes after reading a portion

--- src/smart.cpp
+++ src/smart.cpp
@@ -26,20 +26,20 @@ auto Hash(const std::string& filepath) -> std::uint32_t {
 
 auto Hash(std::istream& input) -> std::uint32_t {
   constexpr static std::size_t BlockSizeElements = 0x100000;
-  constexpr static std::size_t BlockSizeBytes
-      = BlockSizeElements * sizeof(std::uint32_t);
 
   std::uint32_t hash = 0;
   data_processor_t processor;
 
-  std::vector<std::uint32_t> block(BlockSizeElements);
+  std::vector<std::uint32_t> block(BlockSizeElements + 2);
   while (!input.eof()) {
     auto* buffer = reinterpret_cast<char*>(block.data()); // NOLINT
-    input.read(buffer, BlockSizeBytes);
-    block.resize(input.gcount());
+    input.read(buffer, BlockSizeElements * sizeof(std::uint32_t));
+    for (std::size_t i = 0; i < 8; ++i) {
+      buffer[input.gcount() + i] = 0; // NOLINT
+    }
 
     if (!input.eof() && (input.bad() || input.fail())) {
-      throw std::runtime_error("failed during readings input");
+      throw std::runtime_error("failed readings input");
     }
 
     hash = processor.process_block(block);
