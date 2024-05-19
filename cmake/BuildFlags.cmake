2fdf16c9e7f8314e89c1244c73caf0ec983f66c4
vityaman <vityaman.dev@yandex.ru>
2024-05-19 16:29:30 +0300

    Add MSAN support

--- cmake/BuildFlags.cmake
+++ cmake/BuildFlags.cmake
@@ -4,3 +4,11 @@ set(
     CACHE STRING "Compiler flags in ASAN build"
     FORCE
 )
+
+set(
+    CMAKE_CXX_FLAGS_MSAN
+    "-g -fsanitize=memory -fno-omit-frame-pointer \
+    -fno-optimize-sibling-calls -fsanitize-memory-track-origins"
+    CACHE STRING "Compiler flags in MSAN build"
+    FORCE
+)
